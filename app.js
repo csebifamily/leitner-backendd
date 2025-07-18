2b941423d37befeb983d90102182c40302891b37
csebifamily <csebifamily@gmail.com>
2025-07-18 13:14:15 +0200

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://leitner.csebidev.nhely.hu/',
+    origin: 'http://leitner.csebidev.nhely.hu',
     credentials: true
 }));
 
